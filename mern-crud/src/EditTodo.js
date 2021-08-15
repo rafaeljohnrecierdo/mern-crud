4a5cd641dcf4725cdd3fb5e37101a5bb10bd8196
rjrecierdo <45199571+rafaeljohnrecierdo@users.noreply.github.com>
2021-08-15 19:44:55 +0800

    Updated EditTodo.js
    - includes resources, forms, buttons

--- mern-crud/src/EditTodo.js
+++ mern-crud/src/EditTodo.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom"
 
 export const EditTodo = () => {
-    return <div className="container">Edit Todo Item</div>
+  const [todo, setTodo] = useState();
+  const {register, handleSubmit} = useForm({ defaultValues: { text: todo ? todo.text : "" }, });
+  const history = useHistory();
 
-}
+  useEffect(() => {  
+    setTodo({
+      text: "foo"
+    })
+  }, [])
+
+
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+    history.push("/")
+  });
+
+    return(
+    <div className="container">
+      <div className="mt-3">
+        <h3>Edit Todo Item</h3>
+        <form onSubmit ={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text">Text:</label>
+            <input className="form-control" ref={register('email', { required: true })} type="text" name ="text" id="text" />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">Create Todo</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+ );
+};
