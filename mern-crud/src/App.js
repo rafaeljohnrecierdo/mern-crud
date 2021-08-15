71fa31136bc97a2853a379ba3cee86011aecea74
rjrecierdo <45199571+rafaeljohnrecierdo@users.noreply.github.com>
2021-08-15 15:09:04 +0800

    Including Navbar and Routes to app.js

--- mern-crud/src/App.js
+++ mern-crud/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to ="/" className="nav-link">Todos</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to ="/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
+    <Switch>
+      <Route exact path="/" component ={TodoList}/>
+      <Route exact path="/edit/:id" component ={EditTodo}/>
+      <Route exact path="/create" component ={CreateTodo}/>
+    </Switch>
     </div>
   );
 }
